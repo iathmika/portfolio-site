5da5d6e4acb4d6954f79e20eb44ac50591b86277
iathmika <athmikah@gmail.com>
2024-06-19 12:38:58 +0100

    Updates

---
+++ precache-manifest.2626133eb34095d53c08bbde116beb2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b2df611db98bfa8399fcc797b7ae1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b660b4629fa3f634a90",
+    "url": "/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "31a38286a3811de0ca1d",
+    "url": "/static/css/main.81dde03f.chunk.css"
+  },
+  {
+    "revision": "4b660b4629fa3f634a90",
+    "url": "/static/js/2.6f42d8ae.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.6f42d8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a38286a3811de0ca1d",
+    "url": "/static/js/main.23a0de7a.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
