abfaab6f126ceb79785533eed779bfb3cef44379
iathmika <athmikah@gmail.com>
2024-06-19 10:48:15 +0100

    Replace iconify logos for the tech used

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
+import pythonIcon from "@iconify/icons-logos/python";
 import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import jsIcon from "@iconify/icons-logos/javascript";
 
 class About extends Component {
   render() {
@@ -27,11 +27,13 @@ class About extends Component {
                 <span style={{ cursor: "auto" }}>
                   <img
                     height="250px"
+                    
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
+                  {/*  
                   <Icon
-                    icon={angularIcon}
+                    icon={pythonIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
@@ -39,9 +41,10 @@ class About extends Component {
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                   <Icon
-                    icon={vueIcon}
+                    icon={jsIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
+                  */}
                 </span>
               </div>
             </div>
